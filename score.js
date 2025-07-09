03ec238df2098c3a5b4fceeea7f2d1e8b495c030
Tanmay Kakade <tanmay.kakade1416@gmail.com>
2025-07-10 00:29:43 +0530

    final commit

--- score.js
+++ score.js
@@ -1,3 +1,4 @@
+/*
 const p1btn=document.querySelector('#p1button');
 const p2btn=document.querySelector('#p2button');
 const p1Display=document.querySelector('#p1Display');
@@ -54,3 +55,65 @@ function resets(){
     p1Display.textContent=0;
     p2Display.textContent=0;
 }
+*/
+
+
+// Updations
+const p1 = {
+    score: 0,
+    button: document.querySelector('#p1button'),
+    display: document.querySelector('#p1Display')
+}
+const p2 = {
+    score: 0,
+    button: document.querySelector('#p2button'),
+    display: document.querySelector('#p2Display')
+}
+
+const resetButton = document.querySelector('#reset');
+const winningScoreSelect = document.querySelector('#playto');
+let winningScore = 3;
+let isGameOver = false;
+
+function updateScores(player, opponent) {
+    if (!isGameOver) {
+        player.score += 1;
+        if (player.score === winningScore) {
+            isGameOver = true;
+            player.display.classList.add('winner');
+            opponent.display.classList.add('loser');
+            player.button.disabled = true;
+            opponent.button.disabled = true;
+        }
+        player.display.textContent = player.score;
+    }
+}
+
+
+p1.button.addEventListener('click', function () {
+    updateScores(p1, p2)
+})
+p2.button.addEventListener('click', function () {
+    updateScores(p2, p1)
+})
+
+
+winningScoreSelect.addEventListener('change', function () {
+    winningScore = parseInt(this.value);
+    reset();
+})
+
+resetButton.addEventListener('click', reset)
+
+function reset() {
+    isGameOver = false;
+    for (let p of [p1, p2]) {
+        p.score = 0;
+        p.display.textContent = 0;
+        p.display.classList.remove('winner', 'loser');
+        p.button.disabled = false;
+    }
+}
+
+
+/// Thing to add: ager match 5 ki ho aur 4-4 score ho to match 6 ki ho jai jaise actual game me hota hai.
